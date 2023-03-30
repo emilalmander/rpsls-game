f2c7f3f11aed7ac0e9ce620766b4e47bf6c71205
emilalmander <emilalmander@hotmail.se>
2023-03-30 12:13:14 +0000

    added all const needed for the game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /* not my code */ 
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
@@ -35,6 +36,20 @@ function closeModal(modal) {
   modal.classList.remove('active')
   overlay.classList.remove('active')
 }
+
+
+const opponentScore = 0
+const userScore = 0
+const opponentScore_span = document.getElementById("opponent-score");
+const userScore_span = document.getElementById("user-score");
+const score_div = document.querySelector(".score");
+const xwon_div = document.querySelector(".x-won")
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const lizzard_div = document.getElementById("l");
+const smock_div = document.getElementById("sm");
+
 
 
 
