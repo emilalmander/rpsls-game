146ffdd495770cc8cf773fab0c97acbcc8136d86
emilalmander <emilalmander@hotmail.se>
2023-03-31 11:14:26 +0000

    added java so the score increases when winning, also added the java for the game itself

--- script.js
+++ script.js
@@ -38,18 +38,99 @@ function closeModal(modal) {
 }
 
 
-const opponentScore = 0
-const userScore = 0
+let opponentScore = 0
+let userScore = 0
 const opponentScore_span = document.getElementById("opponent-score");
 const userScore_span = document.getElementById("user-score");
+const newGame_div = document.getElementById("new-Game");
 const score_div = document.querySelector(".score");
 const xwon_div = document.querySelector(".x-won")
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 const lizzard_div = document.getElementById("l");
-const smock_div = document.getElementById("sm");
+const smock_div = document.getElementById("sp");
 
 
+function getComputerChoice(){
+  const choices = ["r", "p", "s", "l", "sp"];
+  const randomNumber = Math.floor(Math.random() * 5);
+  return choices[randomNumber];
+}
+function win(){
+  userScore++;
+  userScore_span.innerHTML = userScore;
+}
+function lost(){
+  opponentScore++;
+  opponentScore_span.innerHTML = opponentScore;
+}
+function draw(){
+  console.log("its a draw");
+}
+
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice){
+      case "rs":
+      case "rl":
+      case "lp":
+      case "ls":
+      case "sl":
+      case "sp":
+      case "pr":
+      case "ps":
+      case "sps":
+      case "rsp":
+        win();
+    break;
+      case "rp":
+      case "spr":
+      case "rl":
+      case "ls":
+      case "sps":
+      case "rs":
+      case "ps":
+      case "lp":
+      case "lsp":
+      case "psp":
+        lost();
+        break;
+      case "rr":
+      case "spsp":
+      case "ll":
+      case "ss":
+      case "pp":
+        draw()
+        break;
+  }
+  
+
+}
+
+game("c");
+
+
+
+function main(){
+  rock_div.addEventListener("click", function(){
+  game("r")
+  })
+  paper_div.addEventListener("click", function(){
+  game("p")
+  })
+  scissors_div.addEventListener("click", function(){
+  game("s")
+  })
+  lizzard_div.addEventListener("click", function(){
+  game("l")
+  })
+  smock_div.addEventListener("click", function(){
+  game("sp")
+  })
+}
+
+main();
 
 
