bb741996361068a67a91f7b8d99827cc5f97bc84
emilalmander <emilalmander@hotmail.se>
2023-04-13 12:03:36 +0000

    changed every id tag to class tag in the css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-/* not my code */ 
+/* code influenced by a codepen row 3-38 */ 
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
